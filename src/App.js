b1b098b29ec8b4fe11c5625823ce373257746667
angelhpascual <angelhpascual@gmail.com>
2021-04-11 18:44:00 +0200

    TESTING tailwindcss

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./index.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-red-400">Hello Chuck</h1>
+      <h1 className="text-red-400">Hello Chuck Norris on TEST</h1>
     </div>
   );
 }
