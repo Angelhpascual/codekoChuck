e1b8373538e5c7df304771a9577da2685864aeb4
angelhpascual <angelhpascual@gmail.com>
2021-04-14 19:37:55 +0200

    ADDED --> Loading state and spinner

--- src/pages/quotes.js
+++ src/pages/quotes.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Category from "../components/Categories/Category";
 
 const Quotes = () => {
   const [loading, setLoading] = useState(false);
@@ -16,42 +15,21 @@ const Quotes = () => {
         const { data } = response;
         setCategories(data);
         setLoading(false);
-        return data;
       });
-    axios.get("https://api.chucknorris.io/jokes/random").then((response) => {
-      const { data } = response;
-      setQuotes(data);
-      return data;
-    });
   }, []);
 
-  // const handleQuoteCategory = () => {
-  //   axios
-  //     .get("https://api.chucknorris.io/jokes/random?category=dev")
-  //     .then((response) => {
-  //       const { data } = response;
-  //       console.log(data);
-  //       setQuotes(data);
-  //     });
-  // };
-
-  const handleClick = (category) => {
-    console.log(category.target.value);
+  const getJokeCategory = (category) => {
     axios
       .get(
         `https://api.chucknorris.io/jokes/random?category=${category.target.value}`
       )
-      .then((response) => {
-        const { data } = response;
-        console.log(data.value);
-        setCategoryQuote(data.value);
-        setCategoryQuote((oldArray) => [...oldArray, data.value]);
+      .then((response) => setCategoryQuote(response.data))
+      .catch((error) => {
+        console.error(error);
       });
   };
-
-  console.log(categoryQuote);
   return (
-    <div className="h-screen bg-yellow-500">
+    <div className=" bg-yellow-500">
       <h1 className="text-7xl text-center pt-16 flex flex-col items-center font-mono underline">
         Chuck Norris Quotes
         <img
@@ -60,19 +38,24 @@ const Quotes = () => {
           className="h-44 w-44 mt-8"
         />
       </h1>
-      <div className="grid grid-cols-2 gap-4 container mx-auto pt-12">
-        <div className="bg-yellow-600 h-96 text-center">
-          <div className="grid grid-cols-3 gap-4 p-4">
-            {categories.map((category) => (
-              <button
-                onClick={handleClick}
-                key={category}
-                value={category}
-                className="bg-yellow-300 hover:bg-yellow-400 text-white font-bold py-2 px-4 rounded"
-              >
-                {category}
-              </button>
-            ))}
+      <div className="grid grid-cols-1 gap-4 container mx-auto pt-12">
+        <div className="bg-yellow-600  text-center">
+          <div className="grid grid-cols-4 gap-4 p-4">
+            {loading ? (
+              <div className="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center bg-gray-500 bg-opacity-50">
+                <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-yellow-900"></div>
+              </div>
+            ) : (
+              categories.map((category) => (
+                <button
+                  key={category}
+                  value={category}
+                  className="bg-yellow-300 hover:bg-yellow-400 text-white font-bold py-2 rounded uppercase"
+                >
+                  {category}
+                </button>
+              ))
+            )}
           </div>
         </div>
         <div className="bg-yellow-600 h-96 flex justify-center items-center text-justify text-2xl p-12">
