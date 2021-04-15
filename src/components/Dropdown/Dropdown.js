871a56cf9c03818c2f00f31baa50313bce51de2b
angelhpascual <angelhpascual@gmail.com>
2021-04-15 20:49:20 +0200

    ADDED --> Responsive About page --> DONE

---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Dropdown = ({ isOpen, toggle }) => {
+  return (
+    <div
+      className={
+        isOpen
+          ? "grid grid-rows-4 text-center items-center bg-yellow-400 divide-y-2"
+          : "hidden"
+      }
+      onClick={toggle}
+    >
+      <Link className="p-4 text-yellow-800" to="/">
+        Home
+      </Link>
+      <Link className="p-4 text-yellow-800" to="/about">
+        About
+      </Link>
+      <Link className="p-4 text-yellow-800" to="/quotes">
+        Quotes
+      </Link>
+      <Link className="p-4 text-yellow-800" to="/contact">
+        Contact
+      </Link>
+    </div>
+  );
+};
+
+export default Dropdown;
